a9ad0aabd074d6df8871c972ed3118845d972d03
lcfz86 <zerolcfz86@gmail.com>
2021-06-21 19:07:59 +0800

    Add Update Challenge Week 2

--- challenge-w2.rb
+++ challenge-w2.rb
@@ -6,14 +6,37 @@ name = gets.chomp
 
 puts "Greetings, #{name}"
 
-loop do
-  price_item = []
-  price = ''
-  while price.strip != 'x'
-    print "Enter the item prices or enter x to finish: "
-    price = gets.chomp
-    price_item.push price.to_f
-    puts price == 'x'
-    puts price_item.sum
+price = ''
+answer = ''
+
+in_total = []
+while price.strip != 'x'
+  print "Enter the item prices or enter x to finish: "
+  price = gets.chomp
+  in_total.push price.to_f
+end
+
+# print "Did you have any voucer for 10% discount? (yes/no): "
+# answer = gets.chomp
+# if answer == 'yes'
+#     in_total.sum = in_total.sum * 0.9
+#     puts in_total.sum.to_f
+# elsif answer == 'no'
+#   puts in_total.sum
+# end
+
+puts in_total.sum.to_f
+
+print "Input the payment: "
+payment = gets.chomp.to_f
+balance = payment - in_total.sum
+
+while payment < in_total.sum
+  if payment >= in_total.sum
+    puts "The balace of money is: #{balance}"
+    puts "Thanks for coming."
+  else
+    print "Insufficient payment: "
+    payment = gets.chomp.to_f
   end
 end
